79fa1a0f0a0cfc1ee4ccb88625fa53ab0fbac6de
nv95 <nvasya95@gmail.com>
2016-07-24 12:04:12 +0300

    Add syncmanager

---
+++ app/src/main/java/org/nv95/openmanga/utils/sync/SyncManager.java
@@ -0,0 +1,82 @@
+package org.nv95.openmanga.utils.sync;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.annotation.NonNull;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.TimeZone;
+
+/**
+ * Created by nv95 on 24.07.16.
+ */
+
+public class SyncManager {
+
+    private final Context mContext;
+
+    public SyncManager(Context context) {
+        mContext = context;
+    }
+
+    @NonNull
+    public String getSecurityKey() {
+        PackageManager pm = mContext.getPackageManager();
+        String packageName = mContext.getPackageName();
+        int flags = PackageManager.GET_SIGNATURES;
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = pm.getPackageInfo(packageName, flags);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        Signature[] signatures = packageInfo.signatures;
+        byte[] cert = signatures[0].toByteArray();
+        InputStream input = new ByteArrayInputStream(cert);
+        CertificateFactory cf = null;
+        try {
+            cf = CertificateFactory.getInstance("X509");
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        }
+        X509Certificate c = null;
+        try {
+            c = (X509Certificate) cf.generateCertificate(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String hexString = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            byte[] publicKey = md.digest(c.getEncoded());
+            hexString = byte2HexFormatted(publicKey);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        return hexString;
+    }
+
+    public static long timestampToUTC(long timestamp) {
+        return timestamp - TimeZone.getDefault().getRawOffset();
+    }
+
+    private static String byte2HexFormatted(byte[] arr) {
+        StringBuilder str = new StringBuilder(arr.length * 2);
+        for (int i = 0; i < arr.length; i++) {
+            String h = Integer.toHexString(arr[i]);
+            int l = h.length();
+            if (l == 1) h = "0" + h;
+            if (l > 2) h = h.substring(l - 2, l);
+            str.append(h.toUpperCase());
+            if (i < (arr.length - 1)) str.append(':');
+        }
+        return str.toString();
+    }
+}
